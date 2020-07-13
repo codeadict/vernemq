bd6672f201c6cc69182f2ec17fce122275945b8d
Dairon Medina Caro <dairon@gooee.com>
2020-07-13 11:00:07 -0400

    HTTP test improvements and test for CORS on GET requests

--- apps/vmq_server/test/vmq_http_SUITE.erl
+++ apps/vmq_server/test/vmq_http_SUITE.erl
@@ -1,15 +1,28 @@
 -module(vmq_http_SUITE).
+
+%% CT exports
 -export([
          init_per_suite/1,
          end_per_suite/1,
          init_per_testcase/2,
          end_per_testcase/2,
-         all/0
+         all/0,
+         groups/0
         ]).
 
+%% test case exports
+%% healthcheck endpoint
 -export([
           simple_healthcheck_test/1
         ]).
+
+%% test case exports
+%% security headers
+-export([
+          cors_returned_on_get_request/1
+        ]).
+
+-define(HTTP_HOST, "http://localhost:8888").
 
 init_per_suite(_Config) ->
     cover:start(),
@@ -28,7 +41,24 @@ end_per_testcase(_, Config) ->
     Config.
 
 all() ->
-    [simple_healthcheck_test].
+    [
+        {group, all}
+    ].
+
+groups() ->
+    [
+        {all, [sequence],
+            [
+                {group, health_endpoints},
+                {group, security_headers}
+            ]},
+        %% Health endpoints
+        {health_endpoints, [],
+            [simple_healthcheck_test]},
+        %% Cors et al
+        {security_headers, [],
+            [cors_returned_on_get_request]}
+    ].
 
 simple_healthcheck_test(_) ->
     %% we have to setup the listener here, because vmq_test_utils is overriding
@@ -37,6 +67,20 @@ simple_healthcheck_test(_) ->
                                          {config_mod, vmq_health_http},
                                          {config_fun, routes}]),
     application:ensure_all_started(inets),
-    {ok, {_Status, _Headers, Body}} = httpc:request("http://localhost:8888/health"),
+    {ok, {_Status, _Headers, Body}} = httpc:request(?HTTP_HOST ++ "/health"),
     JsonResponse = jsx:decode(list_to_binary(Body), [return_maps, {labels, binary}]),
     <<"OK">> = maps:get(<<"status">>, JsonResponse).
+
+%% ============================================================
+%% Tests for CORS headers
+%% ============================================================
+
+cors_returned_on_get_request(_) ->
+    vmq_server_cmd:listener_start(8888, [{http, true},
+                                         {config_mod, vmq_http_mgmt_api},
+                                         {config_fun, routes}]),
+    application:ensure_all_started(inets),
+    Endpoint = ?HTTP_HOST ++ "/api/v1/cluster/show",
+    ReqHeaders = [{"origin", "remote.com"}],
+    {ok, {_Status, RespHeaders, _Body}} = httpc:request(get, {Endpoint, ReqHeaders}),
+    "example.com" = proplists:get_value("access-control-allow-origin", RespHeaders).
